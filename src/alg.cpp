7859aa8725f8d8603cde3d482830fbe650dc3722
SolovevNik22IST1 <125061447+SolovevNik22IST1@users.noreply.github.com>
2023-03-31 15:54:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,31 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int left = 0;
+  int right = size;
+  int mid;
+  int new_l, new_r;
+  int res;
+  while (left < right) {
+    mid = (left + right) / 2;
+    if (value > arr[mid]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  new_l = left;
+  left = 0;
+  right = size;
+  while (left < right) {
+    mid = (left + right) / 2;
+    if (value < arr[mid]) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  new_r = right;
+  res = new_r - new_l;
+  return res;
 }
